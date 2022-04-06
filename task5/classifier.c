cc1d2c7eee5668ec13dfa87514d0d5b2e715af55
18chanp1 <18chanp1@gmail.com>
2022-04-06 08:51:02 -0700

    t5 done

--- task5/classifier.c
+++ task5/classifier.c
@@ -30,7 +30,7 @@ void conv1(const double *wts, const double *bias, const double *iacts, double *o
                         for (size_t k = 0; k < CONV1_K; ++k) {
                             for (size_t n = 0; n < batch_sz; ++n) {
                                 oacts[(n * CONV1_K * IN2_X * IN2_Y) + (k * IN2_X * IN2_Y) + (y * IN2_X) + x] +=
-                                    (wts[(k * CONV1_C * CONV1_S * CONV1_R) + (c * CONV1_S * CONV1_R) + (s * CONV1_R) + r] * iacts[(n * CONV1_C * IN1_X * IN1_Y) + (c * IN1_X * IN1_Y) + ((y + s) * IN1_X) + (x + r)]);
+                                        (wts[(k * CONV1_C * CONV1_S * CONV1_R) + (c * CONV1_S * CONV1_R) + (s * CONV1_R) + r] * iacts[(n * CONV1_C * IN1_X * IN1_Y) + (c * IN1_X * IN1_Y) + ((y + s) * IN1_X) + (x + r)]);
                             }
                         }
                     }
@@ -60,15 +60,18 @@ void conv2(const double *wts, const double *bias, const double *iacts, double *o
             }
         }
     }
-    for (size_t r = 0; r < CONV2_R; ++r) {
-        for (size_t s = 0; s < CONV2_S; ++s) {
-            for (size_t x = 0; x < IN3_X; ++x) {
-                for (size_t y = 0; y < IN3_Y; ++y) {
-                    for (size_t c = 0; c < CONV2_C; ++c) {
-                        for (size_t k = 0; k < CONV2_K; ++k) {
-                            for (size_t n = 0; n < batch_sz; ++n) {
-                                oacts[(n * CONV2_K * IN3_X * IN3_Y) + (k * IN3_X * IN3_Y) + (y * IN3_X) + x] +=
-                                    (wts[(k * CONV2_C * CONV2_S * CONV2_R) + (c * CONV2_S * CONV2_R) + (s * CONV2_R) + r] * iacts[(n * CONV2_C * IN2_X * IN2_Y) + (c * IN2_X * IN2_Y) + ((y + s) * IN2_X) + (x + r)]);
+
+    for(i30y = 0; i30y < IN3_Y; i30y += 12){
+        for (size_t n = 0; n < batch_sz; ++n) {
+            for (size_t k = 0; k < CONV2_K; ++k) {
+                for (size_t c = 0; c < CONV2_C; ++c) {
+                    for (size_t x = 0; x < IN3_X; ++x) {
+                        for (size_t y = i30y; y < IN3_Y && y < i30y + 12; ++y) {
+                            for (size_t s = 0; s < CONV2_S; ++s) {
+                                for (size_t r = 0; r < CONV2_R; ++r) {
+                                    oacts[(n * CONV2_K * IN3_X * IN3_Y) + (k * IN3_X * IN3_Y) + (y * IN3_X) + x] +=
+                                            (wts[(k * CONV2_C * CONV2_S * CONV2_R) + (c * CONV2_S * CONV2_R) + (s * CONV2_R) + r] * iacts[(n * CONV2_C * IN2_X * IN2_Y) + (c * IN2_X * IN2_Y) + ((y + s) * IN2_X) + (x + r)]);
+                                }
                             }
                         }
                     }
@@ -76,21 +79,23 @@ void conv2(const double *wts, const double *bias, const double *iacts, double *o
             }
         }
     }
+
+
 }
 
 void maxpool(double *iacts, double *oacts, size_t batch_sz) {
     for (size_t c = 0; c < IN4_C; ++c) {
-	for (size_t x = 0; x < IN4_X; ++x) {
-	    for (size_t y = 0; y < IN4_Y; ++y) {
-		for (size_t n = 0; n < batch_sz; ++n) {
-		    oacts[(n * IN4_C * IN4_X * IN4_Y) + (c * IN4_X * IN4_Y) + (y * IN4_X) + x] =
-			max4(iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 0) * IN3_X) + (x + x + 0)],
-				iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 0) * IN3_X) + (x + x + 1)],
-				iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 1) * IN3_X) + (x + x + 0)],
-				iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 1) * IN3_X) + (x + x + 1)]);
-		}
-	    }
-	}
+        for (size_t x = 0; x < IN4_X; ++x) {
+            for (size_t y = 0; y < IN4_Y; ++y) {
+                for (size_t n = 0; n < batch_sz; ++n) {
+                    oacts[(n * IN4_C * IN4_X * IN4_Y) + (c * IN4_X * IN4_Y) + (y * IN4_X) + x] =
+                            max4(iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 0) * IN3_X) + (x + x + 0)],
+                                 iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 0) * IN3_X) + (x + x + 1)],
+                                 iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 1) * IN3_X) + (x + x + 0)],
+                                 iacts[(n * IN3_C * IN3_X * IN3_Y) + (c * IN3_X * IN3_Y) + ((y + y + 1) * IN3_X) + (x + x + 1)]);
+                }
+            }
+        }
     }
 }
 
@@ -100,10 +105,16 @@ void fc1(const double *wts, const double *bias, const double *iacts, double *oac
             oacts[(n * FC1_K) + k] = bias[k];
         }
     }
-    for (size_t c = 0; c < FC1_C; ++c) {
-        for (size_t k = 0; k < FC1_K; ++k) {
-            for (size_t n = 0; n < batch_sz; ++n) {
-                oacts[(n * FC1_K) + k] += wts[k * FC1_C + c] * iacts[(n * FC1_C) + c];
+    for(size_t n0 = 0; n0 < batch_sz; n0 += 128){
+        for(size_t c0 = 0; c0 < FC1_C; c0 += 128){
+            for(size_t k0 = 0; k0 < FC1_K; k0 += 128){
+                for (size_t n = n0; n < n0 + 128 && n < batch_sz; ++n) {
+                    for (size_t k = k0; k < k0 + 128 && k < FC1_K; ++k) {
+                        for (size_t c = c0; c < c0 + 128 && c < FC1_C; ++c){
+                            oacts[(n * FC1_K) + k] += wts[k * FC1_C + c] * iacts[(n * FC1_C) + c];
+                        }
+                    }
+                }
             }
         }
     }
@@ -123,9 +134,9 @@ void fc2(const double *wts, const double *bias, const double *iacts, double *oac
     }
     for (size_t c = 0; c < FC2_C; ++c) {
         for (size_t k = 0; k < FC2_K; ++k) {
-                for (size_t n = 0; n < batch_sz; ++n) {
-                    oacts[(n * FC2_K) + k] += wts[k * FC2_C + c] * iacts[(n * FC2_C) + c];
-                }
+            for (size_t n = 0; n < batch_sz; ++n) {
+                oacts[(n * FC2_K) + k] += wts[k * FC2_C + c] * iacts[(n * FC2_C) + c];
+            }
         }
     }
 }
